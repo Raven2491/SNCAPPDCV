cce48143bf4e9a9d0ff155eb7c9d4ccf731dfe98
Canales Santos <ecanales@mtc.gob.pe>
2024-10-28 15:54:14 -0500

    Subida45

--- lib/Paginas/mapaentidades2.dart
+++ lib/Paginas/mapaentidades2.dart
@@ -59,7 +59,6 @@ class _MapaEntidades2State extends State<MapaEntidades2> {
               LatLng(double.parse(entidad.latitud),
                   double.parse(entidad.longitud)));
         }
-
         return entidades;
       } else {
         print('Error al obtener datos: ${response.statusCode}');
@@ -79,6 +78,31 @@ class _MapaEntidades2State extends State<MapaEntidades2> {
       }
     }
     return entidadesfiltradas;
+  }
+
+  Future<List<Entidad>> _obtenerEntidadesPorCategoria(String categoria) async {
+    const String url = 'https://endpoint2-blond.vercel.app/entidades';
+    final response = await http.get(Uri.parse(url));
+    try {
+      if (response.statusCode == 200) {
+        List<dynamic> data = json.decode(response.body);
+        entidades = data.map((json) => Entidad.fromJson(json)).toList();
+
+        for (var entidad in entidades) {
+          entidad.proximidad = _calcularDistancia(
+              widget.posicionActual,
+              LatLng(double.parse(entidad.latitud),
+                  double.parse(entidad.longitud)));
+        }
+        return entidades;
+      } else {
+        print('Error al obtener datos: ${response.statusCode}');
+        throw Exception('Error al cargar datos');
+      }
+    } catch (e) {
+      print('Excepción atrapada: $e');
+      throw Exception('Excepción al obtener los datos');
+    }
   }
 
   Future<void> _ubicarEcsal(String nomecsal) async {
@@ -118,109 +142,119 @@ class _MapaEntidades2State extends State<MapaEntidades2> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Entidad>>(
-        future: futureEntidades, // Usamos la variable futureEntidades
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Mapa de entidades',
+            style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto'),
+          ),
+        ),
+        const SizedBox(height: 10),
+        DropdownButtonFormField<String>(
+          isDense: true,
+          onChanged: (value) {
+            // Cambia solo la entidad seleccionada
+            setState(() {
+              selectedEntidad = value;
+            });
+          },
+          decoration: InputDecoration(
+            isDense: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.grey),
+            ),
+          ),
+          items: catEntidades.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
             );
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text('Error al cargar las entidades'),
-            );
-          } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-            entidades = snapshot.data!;
-            filtrarEntidades();
-            return Column(children: [
-              const SizedBox(height: 10),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Mapa de entidades',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto'),
-                ),
-              ),
-              const SizedBox(height: 10),
-              DropdownButtonFormField<String>(
-                isDense: true,
-                onChanged: (value) {
-                  // Cambia solo la entidad seleccionada
-                  setState(() {
-                    selectedEntidad = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  isDense: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Colors.grey),
-                  ),
-                ),
-                items: catEntidades.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                hint: const Text(
-                  "Seleccione una entidad...",
-                  style: TextStyle(fontSize: 14),
-                ),
-                isExpanded: true,
-              ),
-              const SizedBox(height: 10),
-              Expanded(
-                // Usamos Expanded para que ocupe todo el alto disponible
-                child: Container(
-                  width: double.infinity, // Ancho completo
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    child: ecsalespos.isNotEmpty
-                        ? MapaEntidadOpStr(
-                            ubicacion: ecsalespos.first,
-                          )
-                        : widget.posicionActual.latitude == 0 &&
-                                widget.posicionActual.longitude == 0
-                            ? const Center(
-                                child: CircularProgressIndicator(),
-                              )
-                            : MapaEntidadOpStr2(
-                                ubicacion: widget.posicionActual,
-                                entidad: entidades),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MapaEntidades2(
-                                posicionActual: widget.posicionActual,
-                              )));
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                ),
-                child: const Text('Busqueda avanzada',
-                    style: TextStyle(color: Colors.white)),
-              ),
-              const SizedBox(height: 5),
-            ]);
-          } else {
-            return const Center(
-              child: Text('No se encontraron entidades'),
-            );
-          }
-        });
+          }).toList(),
+          hint: const Text(
+            "Seleccione una entidad...",
+            style: TextStyle(fontSize: 14),
+          ),
+          isExpanded: true,
+        ),
+        const SizedBox(height: 10),
+        Expanded(
+          child: FutureBuilder<List<Entidad>>(
+            future: futureEntidades, // Usamos la variable futureEntidades
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: Text('Error al cargar las entidades'),
+                );
+              } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                entidades = snapshot.data!;
+                filtrarEntidades();
+                return Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: double.infinity, // Ancho completo
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          child: ecsalespos.isNotEmpty
+                              ? MapaEntidadOpStr(
+                                  ubicacion: ecsalespos.first,
+                                )
+                              : widget.posicionActual.latitude == 0 &&
+                                      widget.posicionActual.longitude == 0
+                                  ? const Center(
+                                      child: CircularProgressIndicator(),
+                                    )
+                                  : MapaEntidadOpStr2(
+                                      ubicacion: widget.posicionActual,
+                                      entidad: entidades),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: Text('No se encontraron entidades'),
+                );
+              }
+            },
+          ),
+        ),
+        const SizedBox(height: 5),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MapaEntidades2(
+                          posicionActual: widget.posicionActual,
+                        )));
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+          ),
+          child: const Text('Busqueda avanzada',
+              style: TextStyle(color: Colors.white)),
+        ),
+        const SizedBox(height: 10),
+      ],
+    );
   }
 }
